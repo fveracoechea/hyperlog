96b8847247a4525b85beb8d60d42807df1fc517e
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-01-12 21:12:27 -0500

    setup initial schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,4 +1,4 @@
-import { sql } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 import * as t from 'drizzle-orm/sqlite-core';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -24,9 +24,131 @@ export const users = t.sqliteTable('users', {
   lastName: t.text(),
   password: t.text().notNull(),
   email: t.text().notNull().unique(),
+  locale: t.text().default('en'),
   username: t.text().notNull().unique(),
   isActive: t.integer({ mode: 'boolean' }),
   ...timestamps,
 });
 
-// export const userRelations = relations(users, ({ many, one }) => ({}));
+export const usersRelations = relations(users, ({ many }) => ({
+  links: many(links),
+  tags: many(tags),
+  collections: many(usersToCollections),
+}));
+
+export const links = t.sqliteTable('links', {
+  id,
+  url: t.text().notNull(),
+  title: t.text(),
+  description: t.text(),
+  previewImage: t.text(),
+  favicon: t.text(),
+  views: t.integer().default(0),
+  lastVisit: t.text().default(sql`(CURRENT_TIMESTAMP)`),
+  isPinned: t.integer({ mode: 'boolean' }).default(false),
+  collectionId: t
+    .text()
+    .notNull()
+    .references(() => collections.id),
+  ownerId: t
+    .text()
+    .notNull()
+    .references(() => users.id),
+  ...timestamps,
+});
+
+export const linksRelations = relations(links, ({ one, many }) => ({
+  owner: one(users, { fields: [links.ownerId], references: [users.id] }),
+  collection: one(collections, { fields: [links.collectionId], references: [collections.id] }),
+  tags: many(linksToTags),
+}));
+
+export const collections = t.sqliteTable(
+  'collections',
+  {
+    id,
+    name: t.text().notNull(),
+    description: t.text(),
+    color: t.text(),
+    icon: t.text(),
+    order: t.integer().default(1),
+    parentId: t.text().references((): t.AnySQLiteColumn => collections.ownerId),
+    ownerId: t
+      .text()
+      .notNull()
+      .references(() => users.id),
+    ...timestamps,
+  },
+  table => ({
+    uniqueOwnerAndName: t.unique().on(table.name, table.ownerId),
+  }),
+);
+
+export const collectionsRelations = relations(collections, ({ one, many }) => ({
+  owner: one(users, { fields: [collections.ownerId], references: [users.id] }),
+  users: many(usersToCollections),
+}));
+
+export const usersToCollections = t.sqliteTable(
+  'users_to_collections',
+  {
+    id,
+    userId: t
+      .text()
+      .notNull()
+      .references(() => users.id),
+    collectionId: t
+      .text()
+      .notNull()
+      .references(() => collections.id),
+  },
+  table => ({
+    uniqueUserToCollection: t.unique().on(table.userId, table.collectionId),
+  }),
+);
+
+export const usersToCollectionsRelations = relations(usersToCollections, ({ one }) => ({
+  user: one(users, { fields: [usersToCollections.userId], references: [users.id] }),
+  collection: one(collections, {
+    fields: [usersToCollections.collectionId],
+    references: [collections.id],
+  }),
+}));
+
+export const tags = t.sqliteTable(
+  'tags',
+  {
+    id,
+    name: t.text().notNull(),
+    ownerId: t
+      .text()
+      .notNull()
+      .references(() => users.id),
+    ...timestamps,
+  },
+  table => ({
+    uniqueOwnerAndName: t.unique().on(table.name, table.ownerId),
+  }),
+);
+
+export const tagsRelations = relations(tags, ({ one, many }) => ({
+  user: one(users, { fields: [tags.ownerId], references: [users.id] }),
+  links: many(linksToTags),
+}));
+
+export const linksToTags = t.sqliteTable('links_to_tags', {
+  id,
+  linkId: t
+    .text()
+    .notNull()
+    .references(() => links.id),
+  tagId: t
+    .text()
+    .notNull()
+    .references(() => tags.id),
+});
+
+export const linksToTagsRelations = relations(linksToTags, ({ one }) => ({
+  link: one(links, { fields: [linksToTags.linkId], references: [links.id] }),
+  tag: one(tags, { fields: [linksToTags.tagId], references: [tags.id] }),
+}));
