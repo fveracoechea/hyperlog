f39cf182230928c2a821acabedd567a9cf77fee3
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2025-05-23 08:14:45 -0400

    update ui

--- apps/frontend/src/routes/AccountLayout.tsx
+++ apps/frontend/src/routes/AccountLayout.tsx
@@ -2,6 +2,20 @@ import { Outlet } from "react-router";
 import { Header } from "../components/layout/Header.tsx";
 import { Footer } from "../components/layout/Footer.tsx";
 
+import { jsonHash } from "remix-utils/json-hash";
+
+import { client } from "@/utility/honoClient.ts";
+import type { Route } from "./+types/AccountLayout";
+
+export function clientLoader({}: Route.ClientLoaderArgs) {
+  return jsonHash({
+    async ownedCollections() {
+      const response = await client.api.collection.$get({ query: { type: "owned" } });
+      return (await response.json()).data.collections;
+    },
+  });
+}
+
 export default function AccountLayout() {
   return (
     <>
--- apps/frontend/src/routes/AccountPage.tsx
+++ apps/frontend/src/routes/AccountPage.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { jsonHash } from "remix-utils/json-hash";
 import { Route } from "../../.react-router/types/src/routes/+types/AccountPage.ts";
 import { authClient } from "../utility/authClient.ts";
@@ -22,12 +23,14 @@ export function clientLoader({}: Route.ClientLoaderArgs) {
   });
 }
 
-function StatCount(props: { title: string; count: number }) {
-  const { title, count } = props;
+function ListItem(props: { title: string; detail: ReactNode }) {
+  const { title, detail } = props;
   return (
     <li className="flex gap-4 w-full justify-between items-center">
       <Typography>{title}</Typography>
-      <Typography>{count}</Typography>
+      {typeof detail === "string" || typeof detail === "number"
+        ? <Typography>{detail}</Typography>
+        : detail}
     </li>
   );
 }
@@ -36,7 +39,7 @@ export default function AccountPage({ loaderData }: Route.ComponentProps) {
   const { session: { user }, stats } = loaderData;
   return (
     <div className="flex flex-1 max-w-screen-xl w-full mx-auto gap-4">
-      <aside className="flex-1 flex flex-col gap-8 w-full p-4">
+      <aside className="flex-1 flex flex-col gap-6 w-full p-4">
         <div className="flex flex-col justify-center items-center text-center gap-2">
           {user.image
             ? (
@@ -60,9 +63,9 @@ export default function AccountPage({ loaderData }: Route.ComponentProps) {
           <Typography muted variant="small" as="h4">Usage Stats</Typography>
 
           <ul className="flex flex-col gap-2">
-            <StatCount title="Links" count={stats.count.links} />
-            <StatCount title="Collections" count={stats.count.collections} />
-            <StatCount title="Tags" count={stats.count.tags} />
+            <ListItem title="Links" detail={stats.count.links} />
+            <ListItem title="Collections" detail={stats.count.collections} />
+            <ListItem title="Tags" detail={stats.count.tags} />
           </ul>
         </div>
 
@@ -70,6 +73,49 @@ export default function AccountPage({ loaderData }: Route.ComponentProps) {
           <Typography muted variant="small" className="pb-2" as="h4">
             Keyboard Shortcuts
           </Typography>
+          {/* ⌘ */}
+          <ListItem
+            title="Search"
+            detail={
+              <span className="flex gap-1">
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  Ctrl
+                </Typography>
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  K
+                </Typography>
+              </span>
+            }
+          />
+          <ListItem
+            title="Create New"
+            detail={
+              <span className="flex gap-1">
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  Ctrl
+                </Typography>
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  Shift
+                </Typography>
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  O
+                </Typography>
+              </span>
+            }
+          />
+          <ListItem
+            title="Toggle Sidebar"
+            detail={
+              <span className="flex gap-1">
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  Ctrl
+                </Typography>
+                <Typography variant="xsmall" className="bg-cpt-crust rounded-md py-1 px-2">
+                  B
+                </Typography>
+              </span>
+            }
+          />
         </div>
 
         <Button variant="outline">
